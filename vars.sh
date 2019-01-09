80f2951a03b4e48186a45b06d5bbd1082f5b666a
smives <sives10@gmail.com>
2019-01-09 09:36:28 -0700

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 export JRE_URL=https://s3.amazonaws.com/repo.geointservices.io/artifacts/integration/cots/jdk-8u152-linux-x64.tar.gz
-export SONARQUBE_DIST_URL="https://s3.amazonaws.com/repo.geointservices.io/artifacts/dev"
+
+#export SONARQUBE_DIST_URL="https://s3.amazonaws.com/repo.geointservices.io/artifacts/dev"
+export SONARQUBE_DIST_URL="https://binaries.sonarsource.com/Distribution"
 
 export SONARQUBE_ZIP="sonarqube-${SONARQUBE_VERSION}.zip"
